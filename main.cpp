6bcba0ac19b1a982b245b0af9b357ae6bcf7aa2e
WertxGit <pmobileep@gmail.com>
2026-04-19 07:55:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,21 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCLayer.hpp>
-#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
-// Слой для счетчика FPS
 class $modify(FPSLayer, CCLayer) {
-    CCLabelBMFont* m_fpsLabel;
+    CCLabelBMFont* m_label;
 
     bool init() {
         if (!CCLayer::init()) return false;
 
-        // Создаем текст (используем стандартный золотой шрифт)
-        m_fpsLabel = CCLabelBMFont::create("FPS: 60", "goldFont.fnt");
-        m_fpsLabel->setID("fps-label"_spr);
-        
-        // Позиция в углу
-        m_fpsLabel->setAnchorPoint({0, 0});
-        m_fpsLabel->setPosition({5, 5});
-        
-        // Берем масштаб из настроек mod.json
-        float scale = Mod::get()->getSettingValue<double>("font-size");
-        m_fpsLabel->setScale(scale);
+        m_label = CCLabelBMFont::create("FPS: --", "goldFont.fnt");
+        m_label->setScale(0.5f);
+        m_label->setAnchorPoint({0, 0});
+        m_label->setPosition({10, 10});
+        m_label->setID("fps-label"_spr);
 
-        this->addChild(m_fpsLabel, 1000);
+        this->addChild(m_label, 1000);
         this->scheduleUpdate();
 
         return true;
@@ -31,21 +23,9 @@ class $modify(FPSLayer, CCLayer) {
 
     void update(float dt) {
         CCLayer::update(dt);
-        if (m_fpsLabel) {
-            int fps = static_cast<int>(std::round(1.0f / dt));
-            m_fpsLabel->setString(fmt::format("FPS: {}", fps).c_str());
+        if (m_label) {
+            int fps = static_cast<int>(1.f / dt);
+            m_label->setString(fmt::format("FPS: {}", fps).c_str());
         }
-    }
-};
-
-// Модификация главного меню
-class $modify(MyMenuLayer, MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
-
-        // Логируем запуск от твоего имени
-        log::info("WERTX Mod: Меню инициализировано успешно.");
-        
-        return true;
     }
 };
